2835fa83a36f807ec6d1a08ff8660f0e208821a2
Etiennef <etienne-github@outlook.fr>
2016-02-21 19:09:48 +0100

    Adaptation aux évolutions de ConfigManager

--- inc/tabmerger.class.php
+++ inc/tabmerger.class.php
@@ -1,11 +1,12 @@
 <?php
 
 class PluginSmartredirectTabmerger extends PluginConfigmanagerTabmerger {
-	protected static $tabs = array(
-		// '__.*' => 'html code',
-		// CommonGLPI => tabnum|'all',
-		'PluginSmartredirectConfig' => 0,
-		'PluginSmartredirectRule' => 0
-	);
-
+	protected static function getTabsConfig() {
+		return array(
+			// '__.*' => 'html code',
+			// CommonGLPI => tabnum|'all',
+			'PluginSmartredirectConfig' => 0,
+			'PluginSmartredirectRule' => 0
+		);
+	}
 }
